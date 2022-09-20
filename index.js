e10bbf1f74fccff7af90773bc2032785678886bd
Deus <amadeusrm97@gmail.com>
2022-09-20 13:22:41 -0400

    improved documentation

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ class Calculator{
     }
 }
 
+//query searching for all elements in index.html that match the argument
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalButton = document.querySelector('[data-equals]')
@@ -37,11 +38,21 @@ const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
+//creation of a Calculator object
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
+//calls the appropiate method when a specific button is selected
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
+        //this gets the text displayed on that button and calls appendNumber() with that text as argument
         calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
         calculator.updateDisplay()
     })
 })
